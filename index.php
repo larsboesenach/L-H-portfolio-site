b47e131854ecfbc56086b9882df03a4bc147a356
Lars Boesenach <lars@lokaalgevonden.nl>
2015-01-25 13:51:20 +0100

    added the scss folders and some custom test css

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <html>
 <head>
 	<title>lars.hansjan</title>
+	
+	<!-- jQuery link -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	
+	<!-- --------------- -->
+	<!-- Bootstrap	-->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<!-- --------------- -->
+	
+	
+	<!-- Stylesheet -->
+	<link rel="stylesheet/css" type="text/css" href="/css/main.css" />
+
+	<!-- Font -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,400italic' rel='stylesheet' type='text/css'>
 </head>
+
 <body>
 	<p>Hoi.</p>
 </body>
